3c4fedce3ca33831e1dcfdcd8241204979ba4f2b
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2024-11-23 09:55:24 +0100

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,2 +1,2 @@
 // Replace this with your staking contract address
-export const stakingContractAddress = "0x9fB6514bc5048C92c87f4d25371325758f30592A";
+export const stakingContractAddress = "0xe7b226e3038a350f1a925ea5f6b8e8a3a5f3b93a";
